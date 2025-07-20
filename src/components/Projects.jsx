2c3aa7f3708268b9ffb0277ae2a645a01fbb891c
Ayaz <141618127+ayazmallick007@users.noreply.github.com>
2025-07-20 17:34:16 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -485,7 +485,7 @@ const Projects = ({ $reduceMotion }) => {
             title: 'Comming Soon',
             description: 'It will be available soon',
             image: 'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
-            tags: ['React', 'TypeScript', 'Socket.io', 'Express'],
+            tags: ['XXX', 'XXXX', 'XXXX', 'XXXX'],
             github: 'https://github.com',
             link: 'https://example.com',
         },
